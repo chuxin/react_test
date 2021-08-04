aed9152c73256da17765b8bdec122a873ee7fced
Tong Wu <Tong.Wu@worldfirst.com>
2021-08-04 16:35:10 +0800

    company router

--- react_test/react-louis/src/App.js
+++ react_test/react-louis/src/App.js
@@ -1,24 +1,24 @@
-import './bak/App.css';
+// import './bak/App.css';
 import React from 'react';
-import Router from "./router_test/Router";
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//
-//
-//       </header>
-//     </div>
-//   );
-// }
+// import Router from "./router_test/Router";
+import {HashRouter, Route, Link, BrowserRouter} from "react-router-dom";
+import Main from "./Main";
 
 class App extends React.Component {
     render() {
         return (
             <div>
                 <h3>你好啊</h3>
-                <Router></Router>
+                {/*<Router></Router>*/}
+                <BrowserRouter>
+                    <ul>
+                        <li><Link to="/Main">Main</Link></li>
+                        <li><Link to="/Topics">Topics</Link></li>
+                        <li><Link to="/About">About</Link></li>
+                    </ul>
+                    <hr />
+
+                </BrowserRouter>
             </div>
         );
     }
