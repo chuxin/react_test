30fb108d7e0c0cb3ba81883bbe80fc76af50f502
Tong Wu <Tong.Wu@worldfirst.com>
2021-08-06 20:56:05 +0800

    company router

---
+++ react_test/react-louis/src/router_test/AuthButton.js
@@ -0,0 +1,10 @@
+import React, {PureComponent} from "react";
+
+export default class AuthButton extends PureComponent {
+    render () {
+        return (
+            <div>xxxx</div>
+        );
+    }
+}
+
--- react_test/react-louis/src/router_test/Home.js
+++ react_test/react-louis/src/router_test/Home.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import {BrowserRouter, Link, Route} from "react-router-dom";
 import MatchProps from "./MatchProps";
+import About from "./About";
 
-export default class Home extends React.Component {
+class Home extends React.Component {
     render () {
         console.log(this.props.children);
         return (
             <div>
                 <BrowserRouter>
                     <div>
+                        ================  Route三种render方式 之 children方式  &&  路由基本操作   ================
+                        <br /><br />
                         <Link to="/Home">Home</Link><br /><br />
                         <Link to="/Topic">Topic</Link><br /><br />
                         <Link to="/About">About</Link><br /><br />
@@ -19,12 +22,39 @@ export default class Home extends React.Component {
                     </div>
                 </BrowserRouter>
                 <div>
-                    <br /><br /><br />
+                    <br />===============  Route三种render方式 之 component方式，render方式  &&  路由传参  =================
+                    <br /><br />
                     <Link to="/MatchProps">MatchProps</Link><br /><br />
                     {/*Route有三种render方式 之 component方式*/}
                     <Route path="/MatchProps" component={MatchProps} />
                 </div>
+                <div>
+                    <br />=================  登录   ============================
+                    <br /><br />
+                    <AuthButton></AuthButton>
+                    <li>
+                        <Link to="/About">公共页面，无需登录</Link>
+                        <Link to="/private">私有页面，需登录</Link>
+                    </li>
+                    <Route path="/About" component={About}></Route>
+                    <PrivateRoute path="/private" isLogin={} ></PrivateRoute>
+                </div>
+                <div>
+                    <br />=================  先放着   ============================
+                    <br /><br />
+                    <form>
+                        用户名：<input type="text" name="" />
+                        密码：<input type="password" name="passwd"  />
+                        {/*<button onClick={}></button>*/}
+                    </form>
+                </div>
             </div>
         );
     }
+}
+
+export default Home;
+
+class PrivateRoute extends React.Component {
+
 }
